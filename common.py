13aeeb7634fd2bc8b0f24967968167e9902553c3
xirong_li@126.com <xirong.li@gmail.com>
2014-09-24 22:15:46 +0800

    deleted:    VisualSearch/social20/Annotations/Image/concepts20.txt/airplane.txt

--- common.py
+++ common.py
@@ -3,5 +3,5 @@ ROOT_PATH = '/Users/xirong/github/tagrel'
 ROOT_PATH = '/Users/xirong/VisualSearch'
 
 FEATURE_TO_DIM = {'color64':64}
-COLLECTION_TO_SIZE = {'train10k':10000, 'flickr1m':1198818}
+COLLECTION_TO_SIZE = {'social20':19971, 'train10k':10000, 'flickr1m':1198818}
 
